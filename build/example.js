3ee976c1b69923ba915d614c1fde4bece63bdd4d
petebdeblois <petetebdeblois@gmail.com>
2022-08-29 21:09:02 -0400

    let there be light

---
+++ build/example.js
@@ -0,0 +1,15 @@
+// Type Annotations
+let myName = "Me";
+let isTS = true;
+function add(a, b) {
+    return a + b;
+}
+// Inferred Types
+// Union Types
+// Objects and Interfaces
+// Optional Properties
+// Classes
+// Generics
+// any, unknown, never
+// Type Assertions
+// @ts-ignore / @ts-nocheck
---
+++ example.ts
@@ -0,0 +1,26 @@
+// Type Annotations
+
+let myName: string = "Me";
+let isTS: boolean = true;
+
+function add(a: number, b: number) {
+  return a + b
+}
+
+// Inferred Types
+
+// Union Types
+
+// Objects and Interfaces
+
+// Optional Properties
+
+// Classes
+
+// Generics
+
+// any, unknown, never
+
+// Type Assertions
+
+// @ts-ignore / @ts-nocheck
