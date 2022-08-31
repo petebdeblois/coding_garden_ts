420de993a4a2cda599b4c90f02bc308823cc6f9a
petebdeblois <petetebdeblois@gmail.com>
2022-08-31 15:30:46 -0400

    shomer

---
+++ example.ts
@@ -0,0 +1,80 @@
+// Type Annotations
+
+let myName: string = "Me";
+let isTS: boolean = true;
+
+function add(a: number, b: number) {
+  return a + b
+}
+
+// Inferred Types
+
+const numbers = [1, 4, 6, 8, 0]
+
+// Union Types
+
+const numbersAndStrings : (string | number)[] = ["Name", 45, "youhoou", 2]
+
+// twitch 1:21
+
+// Objects and Interfaces
+
+
+
+
+interface Person {
+  name: string;
+  favNumber: number;
+  dogName?: string; // Optionnal
+}
+
+const person = {
+  name: "Stella",
+  favNumber: 2
+ }
+
+function greet(person: Person) {
+  return `Hello ${person.name}`
+}
+
+console.log(greet(person));  // Hello Stella
+
+// Optional Properties 
+
+type StreamerType = "affiliate" | "partner"
+interface People {
+  name: string;
+  favNumber: number;
+  dogName?: string; // Optionnal
+  streamerType: StreamerType
+}
+
+function greetAndDog(people: People) {
+  if (people.dogName) {
+    return `Hello ${people.name} and ${people.dogName}`;
+  }
+  return `Hello ${people.name}`;
+} 
+
+const you: People = {
+  streamerType: "affiliate",
+  name: "Hey",
+  favNumber: 22
+}
+// Classes
+
+// Generics
+
+// any, unknown, never
+
+// Type Assertions
+
+let myOtherName = "CJ";
+
+// myOtherName = 42;
+
+// const x = "hello" as unknown as number;
+
+// x.toFixed();
+
+// @ts-ignore / @ts-nocheck
---
+++ intro_to_react/README.md
@@ -0,0 +1,16 @@
+
+
+
+
+
+## Cheatsheet
+
+https://react-typescript-cheatsheet.netlify.app/
+
+## Create the React app
+
+`npm create vite@latest`
+
+## install linting
+
+`npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint`
